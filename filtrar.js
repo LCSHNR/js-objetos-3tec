4deeaa9a907d9bbabc69e4e490ee68e7e33ca8dd
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2026-06-01 07:42:31 -0300

    aula 5

---
+++ filtrar.js
@@ -0,0 +1,15 @@
+const clientes = require("./cliente.json");
+
+function filtrarApartamentosSemComplementos(clientes){
+    return clientes.filter((cliente) => { 
+        return(
+            cliente.endereco.apartamento && !cliente.endereco.hasOwnProperty("complemento")
+
+        );
+});
+
+}
+
+const filtrados = filtrarApartamentosSemComplementos(clientes);
+
+console.log(filtrados);
