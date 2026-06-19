31f5e68f9fc3447d5903ab9d61be14c50a141fe5
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2026-06-19 13:33:10 +0000

    aa

---
+++ ordenação/listaLivros.js
@@ -0,0 +1,26 @@
+const precoLivros = [
+    {
+    titulo: "JavaScrit",
+    preco: 25
+    },
+     {
+    titulo: "PHP",
+    preco: 25
+    },
+     {
+    titulo: "Java",
+    preco: 25
+    },
+     {
+    titulo: "Elixir",
+    preco: 25
+    },
+     {
+    titulo: "Go",
+    preco: 25
+    },
+     {
+    titulo: "Python",
+    preco: 25
+    },
+]
