dffb60c9662ee8748805533fbf1659a793a108af
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2026-07-30 14:21:01 +0000

    juntaLista

---
+++ ordenação/ordenaçãoI/ordenaçãoII/arrays.js
@@ -0,0 +1,51 @@
+const edFolha = [
+    {
+        titulo: "PHP",
+        preco: 15
+    },
+    {
+        titulo: "JavaScript",
+        preco: 15
+    },
+    {
+        titulo: "Java",
+        preco: 15
+    },
+    {
+        titulo: "Go",
+        preco: 15
+    },
+    {
+        titulo: "Elixir",
+        preco: 15
+    }
+]
+
+const edGalho = [
+    {
+        titulo: "Python",
+        preco: 20
+    },
+    {
+        titulo: "Rust",
+        preco: 22
+    },
+    {
+        titulo: "Ruby",
+        preco: 28
+    },
+    {
+        titulo: "C#",
+        preco: 33
+    },
+    {
+        titulo: "C++",
+        preco: 35
+    },
+    {
+        titulo: "Scala",
+        preco: 40
+    }
+]
+
+module.exports = {edFolha, edGalho}
